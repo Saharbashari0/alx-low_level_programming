cc9eae2f6948e7a51d6fb37824193583602ecd1a
Saharbashari0 <saharbashari07@gmail.com>
2023-08-11 17:36:54 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: 'print numbers of base 10'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+		return (0);
+	}
+
