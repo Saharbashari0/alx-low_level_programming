3718753b392b723b8c99cb21a44217a5a9cba777
Saharbashari0 <saharbashari07@gmail.com>
2023-09-01 09:53:27 -0700

    updated

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * Letter Another cases, shows 0
+ * @c: The character to be checked
+ *
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+	return (1);
+}
+else
+	{
+		return (0);
+	}
+_putchar('\n');
+}
